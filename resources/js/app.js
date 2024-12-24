24ef9b6b28ca784d02dd821e7fa3a58533cdab3f
ffabi00 <fabio-cr@live.com>
2024-12-24 16:27:16 -0300

    Adiciona roteamento com Vue Router e implementa troca de fundo dinâmico na aplicação.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,45 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { Quasar } from 'quasar'
+import router from './router'
+import '../css/styles.sass'
+
 // Import icon libraries
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
 
 // Import Quasar css
 import 'quasar/src/css/index.sass'
+
+const backgrounds = [
+    '/images/marvel_background_1.jpg',
+    '/images/marvel_background_2.jpg',
+    '/images/marvel_background_3.jpg',
+    '/images/marvel_background_4.jpg'
+]
+
+let currentBackgroundIndex = 0
+
+function changeBackground () {
+    const backgroundElements = document.querySelectorAll('.background-grid div')
+    backgroundElements.forEach((element, index) => {
+        const backgroundIndex = (currentBackgroundIndex + index) % backgrounds.length
+        element.style.backgroundImage = `url(${backgrounds[backgroundIndex]})`
+    })
+    currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const backgroundGrid = document.createElement('div')
+    backgroundGrid.className = 'background-grid'
+    for (let i = 0; i < 4; i++) {
+        const div = document.createElement('div')
+        backgroundGrid.appendChild(div)
+    }
+    document.body.appendChild(backgroundGrid)
+    changeBackground()
+    setInterval(changeBackground, 30000) // 30 segundos
+})
 
 createInertiaApp({
     resolve: name => {
@@ -19,6 +52,7 @@ createInertiaApp({
             .use(Quasar, {
                 plugins: {},
             })
+            .use(router)
             .mount(el)
     },
 })
